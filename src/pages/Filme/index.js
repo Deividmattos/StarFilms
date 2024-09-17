e255b1005bee9e6b117ad199a1958f92a52e61b0
devDeivid <deividmattos97@gmail.com>
2024-09-17 20:32:23 +0100

    Buscando Trailer no Youtube

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+
+import "./filme-info.css";
 
 import api from "../../services/api";
 
 function Filme() {
   const { id } = useParams();
+
+  const navigate = useNavigate();
+
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -23,6 +28,8 @@ function Filme() {
         })
         .catch(() => {
           console.log("FILME NAO ENCONTRADO");
+          navigate("/", { replace: true });
+          return;
         });
     }
 
@@ -31,7 +38,7 @@ function Filme() {
     return () => {
       console.log("COMPONENTE FOI DESMONTADO");
     };
-  }, []);
+  }, [navigate, id]);
 
   if (loading) {
     return (
@@ -52,7 +59,20 @@ function Filme() {
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
 
-      <strong>Avalição: {filme.vote_average} / 10</strong>
+      <strong>Avalição: {filme.vote_average} / 20</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a
+            target="blank"
+            rel="external"
+            href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
